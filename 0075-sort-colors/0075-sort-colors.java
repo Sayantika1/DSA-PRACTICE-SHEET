ad3ef6bfc0d5f0b9c82b4b0cac519ce68264938c
Sayantika Sadhukhan <128018824+Sayantika1@users.noreply.github.com>
2026-07-24 10:50:22 +0530

    Time: 1 ms (24.09%), Space: 43.8 MB (11.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,44 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        mergesort(nums, 0, nums.length-1);
     }
+
+    public void mergesort(int[] nums, int left, int right)
+    {
+        if(left >= right)
+        {
+            return;
+            
+        }
+        int mid= (left + right )/2;
+        mergesort(nums, left ,mid );
+        mergesort(nums, mid+1, right);
+        merge(nums,left, mid, right);
+    } 
+
+    public void merge(int[] arr, int low, int mid, int high)
+    {
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.add(arr[left++]);
+            else
+                temp.add(arr[right++]);
+        }
+
+        while (left <= mid)
+            temp.add(arr[left++]);
+
+        
+        while (right <= high)
+            temp.add(arr[right++]);
+
+        
+        for (int i = low; i <= high; i++)
+            arr[i] = temp.get(i - low);
+    }
+
+   
 }
